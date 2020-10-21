c52dfbdef5a8b34924d3681858d8fd2e1e193555
João Krüger <jgckruger@gmail.com>
2020-10-21 17:04:59 -0300

    New build script

--- scripts/0-build-images.sh
+++ scripts/0-build-images.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-export SPARK_HOME
-export HADOOP_HOME
+# TODO: get args to build up on
+# TODO: check if spark is installed
+$SPARK_HOME/bin/docker-image-tool.sh -p $SPARK_HOME/kubernetes/dockerfiles/spark/bindings/python/Dockerfile -r jgckruger -t v3.0.1 build
+docker push jgckruger/spark:v3.0.1
+docker push jgckruger/spark-py:v3.0.1
+docker build --pull --rm -f "../spark/driver-pod/Dockerfile" -t jgckruger/my-notebook:latest "driver-pod"
+docker push jgckruger/spark-py:v3.0.1
