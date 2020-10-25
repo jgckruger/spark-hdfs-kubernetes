c0dee182f90899d5da17ae91353d271dda512265
João Krüger <jgckruger@gmail.com>
2020-10-25 19:55:49 -0300

    Added new commands to build script

--- scripts/0-build-images.sh
+++ scripts/0-build-images.sh
@@ -4,5 +4,7 @@
 $SPARK_HOME/bin/docker-image-tool.sh -p $SPARK_HOME/kubernetes/dockerfiles/spark/bindings/python/Dockerfile -r jgckruger -t v3.0.1 build
 docker push jgckruger/spark:v3.0.1
 docker push jgckruger/spark-py:v3.0.1
-docker build --pull --rm -f "../spark/driver-pod/Dockerfile" -t jgckruger/my-notebook:latest "driver-pod"
+docker build --pull --rm -f "../spark/driver-pod/Dockerfile" -t jgckruger/my-notebook:latest "../spark/driver-pod"
+docker push jgckruger/spark:v3.0.1
 docker push jgckruger/spark-py:v3.0.1
+docker push jgckruger/my-notebook:v3.0.1
