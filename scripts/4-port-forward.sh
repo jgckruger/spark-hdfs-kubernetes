f2ce0c9e7ba564d5d370d7e89dff4eb1e7921598
João Krüger <jgckruger@gmail.com>
2020-11-01 15:11:02 -0300

    Fixed service on open port script

--- scripts/4-port-forward.sh
+++ scripts/4-port-forward.sh
@@ -4,7 +4,7 @@ is_spark_environment=$(kubectl get namespaces | grep 'spark' | wc -l)
 #TODO: wait for container creation
 if [[ is_hadoop_environment -eq "1" ]] # if hdfs is running
 then
-    kubectl port-forward -n hadoop service/hadoop-hadoop-yarn-ui 9000:9000 &
+    kubectl port-forward -n hadoop service/hadoop-hadoop-hdfs-nn 9000:9000 &
     kubectl port-forward -n hadoop service/hadoop-hadoop-yarn-ui 8088:8088 &
     kubectl port-forward -n hadoop service/hadoop-hadoop-hdfs-nn 50070:50070 &
     kubectl port-forward -n hadoop $(kubectl get pod -n hadoop --selector=app=zeppelin -o jsonpath='{.items...metadata.name}') 8080:8080 &
