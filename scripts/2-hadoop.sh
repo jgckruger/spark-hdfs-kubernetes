c422bd04535e515b1e6b4952499f7888284d52ce
João Krüger <kruger.joao@ppgia.pucpr.br>
2020-11-14 17:19:41 -0300

    Fixed sh for windows

--- scripts/2-hadoop.sh
+++ scripts/2-hadoop.sh
@@ -3,10 +3,8 @@ kubectl create serviceaccount hadoop -n hadoop
 kubectl create clusterrolebinding hadoop-role --clusterrole=edit --serviceaccount=hadoop:hadoop --namespace=hadoop
 kubectl apply -f ../hadoop/hadoop/volumes/pv-volume.yaml
 kubectl apply -f ../hadoop/hadoop/volumes/pv-claim.yaml
-helm install hadoop ../hadoop/hadoop-1.1.2.tgz  \
-        --version 1.1.2  \
-        --namespace hadoop 
+helm install hadoop ../hadoop/hadoop-1.1.2.tgz --version 1.1.2 --namespace hadoop 
+kubectl get all -n hadoop
 #helm install hadoop ../hadoop --version 1.1.2 --namespace hadoop # https://artifacthub.io/packages/helm/helm-stable/hadoop
 #helm install hadoop $(stable/hadoop/tools/calc_resources.sh 70) stable/hadoop
 #helm install zeppelin --namespace hadoop --set hadoop.useConfigMap=true,hadoop.configMapName=hadoop-hadoop stable/zeppelin # https://artifacthub.io/packages/helm/helm-stable/zeppelin
-kubectl get all -n hadoop
